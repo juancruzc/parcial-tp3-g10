418f5f96a5ab06845a3614a792bdd393c1bbe42a
André Chaumet <andrechaumetcutuli@gmail.com>
2024-10-12 20:51:22 -0300

    Added BarButton, Added compose material import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.material:material:1.4.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/example/parcialtp3grupo10/ui/components/ButtonBar.kt
@@ -0,0 +1,39 @@
+package com.example.parcialtp3grupo10.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun ButtonBarPreview() {
+    MaterialTheme {
+        ButtonBar("Add all to cart")
+    }
+}
+
+@Composable
+fun ButtonBar(title: String) {
+    Button(
+        onClick = {  },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(95.dp)
+            .padding(16.dp),
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(Color(0xFF53B175))
+    ) {
+        Text(title, color = Color.White, fontSize = 20.sp)
+    }
+}
