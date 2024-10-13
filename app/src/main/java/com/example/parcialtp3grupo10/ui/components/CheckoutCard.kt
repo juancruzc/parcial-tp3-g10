2daa216eea2f17de387445cfbedd63bdc9232e62
juancruzc <juancruzcobo@hotmail.com>
2024-10-13 18:00:02 -0300

    feat: Success screen and popups

--- app/src/main/java/com/example/parcialtp3grupo10/ui/components/CheckoutCard.kt
+++ app/src/main/java/com/example/parcialtp3grupo10/ui/components/CheckoutCard.kt
@@ -2,23 +2,20 @@ package com.example.parcialtp3grupo10.ui.components
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
@@ -29,8 +26,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,8 +34,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.parcialtp3grupo10.R
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 
@@ -50,14 +43,12 @@ fun CheckoutCard(
     price: Double,
     modifier: Modifier = Modifier
 ) {
-    var quantity by remember { mutableStateOf(1) }
-
     Card(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White),
-        shape = RoundedCornerShape(12.dp),
+        shape = RoundedCornerShape(16.dp),
     ) {
         Column(
             modifier = Modifier
@@ -96,7 +87,7 @@ fun CheckoutCard(
             // Payment Option
             CheckoutRowWithIcon(
                 label = "Payment",
-                painter = painterResource(id = R.drawable.payment), // Replace with actual icon resource
+                painter = painterResource(id = R.drawable.card),
                 iconDescription = "Payment Method"
             )
 
@@ -200,10 +191,9 @@ fun CheckoutRowWithIcon(label: String, painter: Painter, iconDescription: String
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun CheckoutCardPreview() {
-    val imagePainter = painterResource(id = R.drawable.apple)
     MaterialTheme {
         CheckoutCard(
             price = 1.99,
