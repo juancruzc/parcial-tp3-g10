95ccdee3a1dc5aa64c1c7ba9a5653fa711a00274
juancruzc <juancruzcobo@hotmail.com>
2024-10-14 20:35:17 -0300

    fix: cart navigation

--- app/src/main/java/com/example/parcialtp3grupo10/MainActivity.kt
+++ app/src/main/java/com/example/parcialtp3grupo10/MainActivity.kt
@@ -3,15 +3,11 @@ package com.example.parcialtp3grupo10
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.padding
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -70,6 +66,9 @@ class MainActivity : ComponentActivity() {
                         }
                         composable("account") {
                             AccountScreen(navController)
+                        }
+                        composable("success") {
+                            SuccessScreen(navController)
                         }
                     }
                 }
