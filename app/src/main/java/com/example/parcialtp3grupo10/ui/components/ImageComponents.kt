29086a4f3e5b830c93814f26d445719842d35871
calvet11 <145401962+calvet11@users.noreply.github.com>
2024-10-15 14:57:52 -0300

    nada

--- app/src/main/java/com/example/parcialtp3grupo10/ui/components/ImageComponents.kt
+++ app/src/main/java/com/example/parcialtp3grupo10/ui/components/ImageComponents.kt
@@ -2,10 +2,13 @@ package com.example.parcialtp3grupo10.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.parcialtp3grupo10.R
 
 @Composable
@@ -33,7 +36,9 @@ fun BottomCenteredImage(modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(id = R.drawable.img_2),
         contentDescription = "Image at the bottom",
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth(0.5f) // Ajusta el ancho al 50% de la pantalla
+            .height(50.dp), // Ajusta la altura según sea necesario
         contentScale = ContentScale.Crop
     )
 }
